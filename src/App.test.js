1dc6ef17e14b19f0818078c183e5601c6a416cf8
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-10-11 17:23:06 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 test("renders todo app", () => {
@@ -13,11 +14,11 @@ test("adds and deletes a todo", () => {
 
   // Type a todo in the input field
   const inputElement = screen.getByRole("textbox");
-  fireEvent.change(inputElement, { target: { value: "Test Todo" } });
+  userEvent.type(inputElement, "Test Todo");
 
   // Click the "Create Todo" button
   const createButton = screen.getByText(/Create Todo/i);
-  fireEvent.click(createButton);
+  userEvent.click(createButton);
 
   // Verify that the todo is added to the list
   const todoElement = screen.getByText(/Test Todo/i);
@@ -25,7 +26,7 @@ test("adds and deletes a todo", () => {
 
   // Click the "Delete" button
   const deleteButton = screen.getByText(/Delete/i);
-  fireEvent.click(deleteButton);
+  userEvent.click(deleteButton);
 
   // Verify that the todo is deleted from the list
   expect(todoElement).not.toBeInTheDocument();
